60cbbfcc419839b63d025f3c360979ec6200fac5
Nguyen Hoang <hoangng@mor.com.vn>
2021-02-17 15:44:34 +0900

    update

--- src/utility/browserStorageUtil.ts
+++ src/utility/browserStorageUtil.ts
@@ -1,5 +1,5 @@
-export const getLocalStorage = (itemName: string) => localStorage.getItem(itemName) || '';
+export const getLocalStorage = (itemName: string) => JSON.parse(localStorage.getItem(itemName) || '{}');
 export const setLocalStorage = (itemName: string, value: string) => { localStorage.setItem(itemName, value); };
 
-export const getSessionStorage = (itemName: string) => sessionStorage.getItem(itemName) || '';
+export const getSessionStorage = (itemName: string) => JSON.parse(sessionStorage.getItem(itemName) || '{}');
 export const setSessionStorage = (itemName: string, value: string) => { sessionStorage.setItem(itemName, value); };
